beacb75dc9527ea269502dfe1184bf2d73f84311
Tony <noobert@gmail.com>
2013-11-05 09:00:08 -0600

    Init check in

---
+++ server.js
@@ -0,0 +1,45 @@
+/**
+ * Created by tony.gemoll on 11/1/13.
+ */
+var http = require('http');
+var sockjs = require('sockjs');
+var Hapi = require('hapi');
+
+var port = process.env.port || 9999;
+
+// 1. Echo sockjs server
+var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
+
+var connections = [];
+
+var sockjs_echo = sockjs.createServer(sockjs_opts);
+sockjs_echo.on('connection', function(conn) {
+    conn.on('data', sendMessage);
+    connections.push(conn);
+
+});
+
+function sendMessage(message){
+    for(var x = 0; x < connections.length; x++)
+    {
+        connections[x].write(message);
+    }
+}
+
+
+var hapi_server = Hapi.createServer('0.0.0.0', port);
+
+hapi_server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+        directory: { path: './html', listing: false, index: true }
+    }
+});
+
+sockjs_echo.installHandlers(hapi_server.listener, {prefix:'/echo'});
+
+console.log(' [*] Listening on 0.0.0.0:9999' );
+hapi_server.start()
+
+
