45a0f34e2efd75f13add8f0d6c5f623774926ce3
Tony <noobert@gmail.com>
2013-12-20 08:49:17 -0600

    Updated sendBoardTo to use streams. Updated objects to be more inline with what they represent.

--- server.js
+++ server.js
@@ -1,59 +1,49 @@
-/**
- * Created by tony.gemoll on 11/1/13.
- */
 var http = require('http');
 var sockjs = require('sockjs');
 var Hapi = require('hapi');
 var levelup = require('levelup');
-var events = require('events');
+
 
 var db = levelup('./db');
 var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
 var boardPrefix =  'board';
 var sockjsServer = sockjs.createServer(sockjs_opts);
-var moveEmitter = new events.EventEmitter();
 
-
+db.on('error', logError);
 
 sockjsServer.on('connection', function(conn) {
     conn.on('data', sendMessage);
 
-    var moveFun = function(move){
-        conn.write(JSON.stringify(move));
+    var moveFun = function(key, value){
+        conn.write(value);
     };
 
     conn.on('close', function() {
-        moveEmitter.removeListener('move', moveFun);
+        db.removeListener('put', moveFun);
     });
-    moveEmitter.on('move', moveFun);
+    db.on('put', moveFun);
 
     sendBoardTo(conn);
 });
 
 function sendBoardTo(conn) {
 
-    db.createReadStream({
+    db.createValueStream({
         start: boardPrefix,
         end: boardPrefix + '\xFF'
-    })
-    .on('data', function (data) {
-        conn.write(JSON.stringify(data));
-    });
+    }).pipe(conn, { end : false });
+
+
 }
 
 function sendMessage(message){
-
     var move = JSON.parse(message);
-    move.key = boardPrefix + move.key;
-    db.put(move.key, move.value, logError);
-
-    moveEmitter.emit('move', move);
+    var moveKey = boardPrefix + move.tile;
+    db.put(moveKey, message);
 }
 
-
-function logError(err)
-{
-    if (err) console.log('Ooops!', err);
+function logError(err){
+    if (err) console.log('levelDB had an error!', err);
 }
 
 
@@ -73,3 +63,4 @@ hapi_server.route({
 sockjsServer.installHandlers(hapi_server.listener, {prefix:'/echo'});
 
 hapi_server.start();
+
